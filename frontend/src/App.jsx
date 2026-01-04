48cc6e734ae51c95d549857a2f815e0e7fed08c2
noraaren <aaren3017@gmail.com>
2026-01-04 18:16:24 -0500

    added login page for supabase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import StartPage from "./StartPage.jsx";
 import MainPage from "./MainPage.jsx";
+import LoginPage from "./loginPage.jsx";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<StartPage />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/Start" element={<StartPage />} />
       <Route path="/MainPage" element={<MainPage />} />
     </Routes>
   );
